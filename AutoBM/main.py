00116fd551b742886dd5c4314c42f80fa019b067
bonaktan <78548520+bonaktan@users.noreply.github.com>
2022-12-11 17:50:48 +0800

    Monitoring Chats Successful

--- AutoBM/main.py
+++ AutoBM/main.py
@@ -1,22 +1,33 @@
-# Main Thread
 try:
     from selenium import webdriver
     from selenium.webdriver.common.by import By
+    from selenium.webdriver.firefox.options import Options
 except ImportError: raise ImportError("Selenium not found")
+
+try: from bs4 import BeautifulSoup as bs4
+except: raise ImportError("bs4 not found")
 import time
 from configparser import ConfigParser
+
 
 def configread(): # reads the config.ini files for use
     config = ConfigParser()
     config.read("config.ini")
     return config
+def listofconvo(driver):
+    convo = list(bs4(
+        driver.find_elements(By.XPATH,
+            '//div[@role="grid"]')[1].get_attribute("outerHTML"),
+        "html.parser").findChild().children)
 def _printhtml(x): print(x.get_attribute("innerHTML")) # DEBUG FUNCTION
 
-def main():
+
+def main(debug=False):
     # for termux, make sure vncserver is working properly,
-    # TODO: add code for headless mode (after everything is done)
-    with webdriver.Firefox() as driver:
+    options = Options(); options.headless = not debug
+    with webdriver.Firefox(options=options) as driver:
         driver.install_addon("plugins/ublock_origin.xpi", temporary=True) # DEBUG
+
         driver.get("https://www.messenger.com/login")
         config = configread()
         form = driver.find_element(By.ID, "login_form") # returns a <form> element
@@ -25,9 +36,13 @@ def main():
             form.find_element(By.NAME, "email"), config["Login"]["Email"],
             form.find_element(By.NAME, "pass"), config["Login"]["Password"])
         form.submit()
+        time.sleep(5)
+        driver.get("https://www.messenger.com/t/4576805812442937")
         time.sleep(30) # Termux-Specific Delay, should be closer to 15seconds
-        print("Test Complete")
-        breakpoint()
-        # Problem: monitor the chats
-        while True: pass # DEBUG
-if __name__ == "__main__": main()
+
+        print("Monitoring")
+        oldmessages = listofconvo(driver)
+        while True:
+            newmessages = listofconvo(driver)
+            if oldmessages != newmessages: print("UPDATE"); newmessages=oldmessages
+if __name__ == "__main__": main(debug=True)
