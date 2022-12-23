389feff0f881bc6ca33ca9e90ea55fb0fddd5edd
bonaktan <78548520+bonaktan@users.noreply.github.com>
2022-12-23 18:26:59 +0800

    Update Script Init

---
+++ update.py
@@ -0,0 +1,14 @@
+import requests
+
+main = requests.get("https://raw.githubusercontent.com/bonaktan/AutoBM/main/AutoBM/main.py")
+update = requests.get("https://raw.githubusercontent.com/bonaktan/AutoBM/main/update.py")
+
+print(
+    """WARNING: You WILL Overwrite the main.py and update.py files, any changes WILL
+be lost, do you wish to continue? (y/N)  """, end="")
+response = str(input())
+if response.lower() != "y": print("Aborting"); quit()
+
+with open("AutoBM/main.py", "w") as m: m.write(main.text)
+with open("update.py", "w") as m: u.write(update.text)
+
