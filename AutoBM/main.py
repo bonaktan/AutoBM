28a87fa2a8864966c50231a640de322f6ef5d6e7
bonaktan <78548520+bonaktan@users.noreply.github.com>
2022-12-24 12:12:52 +0800

    Revert "Optimized Bootup Standby"
    This reverts commit fd9fc3e129ca9b60e712e08cfd38f663cbe804e0.

--- AutoBM/main.py
+++ AutoBM/main.py
@@ -3,26 +3,24 @@ try:
     from selenium.webdriver.common.by import By
     from selenium.webdriver.firefox.options import Options
 except ImportError: raise ImportError("Selenium not found")
+
 try: from bs4 import BeautifulSoup as bs4
 except: raise ImportError("bs4 not found")
 import time
 from configparser import ConfigParser
 from pdb import set_trace as breakpoint
 from pdb import post_mortem
-import logging
-
 def configread(): # reads the config.ini files for use
     config = ConfigParser()
     config.read("config.ini")
     return config
-
 def listofconvo(driver):
     convo = bs4(driver.find_elements(By.XPATH,
             '//div[@role="grid"]')[1].get_attribute("outerHTML"),
             "html.parser").findChild().children
-    """
-    TODO: convert all of this to javascript equivalents
-    """
+    # first 2 lines of code selects the container containing the convo
+    # last line bs4-ises it, then selects the indiv. chat containers
+    # breakpoint()
     convo = [[i.text.strip() for i in chat.findChild().findChild().children
               if i.text != ""]
              for chat in convo] # NO MEDIA, IMPROVE
@@ -31,34 +29,30 @@ def listofconvo(driver):
 def _printhtml(x): print(x.get_attribute("innerHTML")) # DEBUG FUNCTION
 
 
-def main():
+def main(debug=False):
     # for termux, make sure vncserver is working properly,
-    config = configread(); debug = config["Developer"].getboolean("Debug")
-    options = Options()
-    logging.basicConfig(level=logging.INFO if debug else logging.WARNING)
-    options.headless = not config["Developer"].getboolean("Debug")
-    logging.info(f"Launching Firefox {'in debug' if debug else ''}")
+    options = Options(); options.headless = not debug
+    print(f"Launching Firefox {'in debug' if debug else ''}")
     with webdriver.Firefox(options=options) as driver:
         driver.install_addon("plugins/ublock_origin.xpi", temporary=True) # DEBUG
-        logging.info("Logging In")
+        print("Launched Firefox")
+
         driver.get("https://www.messenger.com/login")
+        print("Fetched Messenger")
+        config = configread()
         form = driver.find_element(By.ID, "login_form") # returns a <form> element
         driver.execute_script(
             "arguments[0].value = arguments[1]; arguments[2].value = arguments[3];",
             form.find_element(By.NAME, "email"), config["Login"]["Email"],
             form.find_element(By.NAME, "pass"), config["Login"]["Password"])
+        print("Signing In Messenger")
         form.submit()
-        # TODO: assert that the credentials entered are correct
         time.sleep(5)
+        print("Loading Converdation")
+        driver.get("https://www.messenger.com/t/4576805812442937")
+        time.sleep(45) # Termux-Specific Delay, should be closer to 15seconds
 
-        driver.get(config["Chats"]["MonitorChat"])
-        logging.info("Loading Convo")
-        while True:
-            try: driver.find_elements(By.XPATH,'//div[@role="grid"]')[1]
-            except: continue
-            break
-        time.sleep(2)
-        logging.info("Loaded Convo")
+        print("Monitoring")
         try:
             convo = listofconvo(driver)
             old = convo[-1]
@@ -70,8 +64,4 @@ def main():
             import traceback
             traceback.format_exc()
             post_mortem()
-        """
-        TODO: The whole loop is computationally expensive, use queue to process shit
-        properly
-        """
-if __name__ == "__main__": main()
+if __name__ == "__main__": main(debug=False)
